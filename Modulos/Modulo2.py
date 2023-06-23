d235e4331d0e1014232c4a6db699dc810cb4120f
KarimDevTech <karim.dev.tech@gmail.com>
2023-06-23 18:22:44 -0300

    Se agrega el modulo2

---
+++ Modulos/Modulo2.py
@@ -0,0 +1,14 @@
+'''
+-----------------------------------------------------------
+FUNCION QUE RECIBE LISTAS Y LAS CONVIERTE A ARRAYS DE NUMPY
+-----------------------------------------------------------
+'''
+
+import numpy as np
+#Crear un array a partir de las listas
+def crear_arrays(a):
+    arr = np.array(a)
+    return arr
+
+
+    
